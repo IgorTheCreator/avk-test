01cfc1028b3baf8ef326c5c5b0ef75bee9340b67
Morozov Igor <morozovigorq@gmail.com>
2024-10-02 20:00:29 +0300

    feat: implement forRootAsync static function for first module

--- src/first/first.module.ts
+++ src/first/first.module.ts
@@ -1,5 +1,6 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { FirstService } from './first.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [FirstService],
@@ -16,6 +17,24 @@ export class FirstModule {
         FirstService,
       ],
       exports: [FirstService],
+    };
+  }
+
+  static forRootAsync(): DynamicModule {
+    return {
+      module: FirstModule,
+      imports: [ConfigModule.forRoot()],
+      providers: [
+        {
+          provide: 'FIRST_NUMBER',
+          useFactory: async (configService: ConfigService) => {
+            return configService.get<number>('FIRST_NUMBER', 10);
+          },
+          inject: [ConfigService],
+        },
+        FirstService,
+      ],
+      exports: ['FIRST_NUMBER', FirstService],
     };
   }
 }
